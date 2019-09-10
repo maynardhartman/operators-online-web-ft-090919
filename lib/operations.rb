c6e23953f1e1097f45613b7e8fac2f6c305061ef
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-10 16:48:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if ( speed > 60 || speed < 40 )
+    return TRUE
+  else 
+    return FALSE
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? retrun TRUE : return FALSE
 end
 	
 
